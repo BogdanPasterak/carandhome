21915e2b6cd9661de1a8eb8f79d128a03cf5e4b5
bogdanpasterak <bogdanpasterak@gmail.com>
2020-02-09 20:29:14 +0000

    catalog

---
+++ carandhome/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<?php
+include(page2.php);
+?>
+
+<html lang="en">
+   <head>
+
+      <meta charset="utf-8">
+      <title>Home & Car</title>
+
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="description" content="Description for browser">
+      <meta name="keywords" content="">
+      <meta name="author" content="Bogdan Pasterak">
+
+      <link rel="stylesheet" href="style.css">
+      <script src="script.js"></script>
+
+   </head>
+   <body>
+<?php
+echo "My first PHP script!";
+?>
+      <a href="../index.html">
+         <h2>Site under construction</h2>
+      </a>
+
+   
+
+
+
+   </body>
+
+</html>
---
+++ carandhome/page2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+   <head>
+
+      <meta charset="utf-8">
+      <title>Home & Car</title>
+
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="description" content="Description for browser">
+      <meta name="keywords" content="">
+      <meta name="author" content="Bogdan Pasterak">
+
+      <link rel="stylesheet" href="style.css">
+      <script src="script.js"></script>
+
+   </head>
+   <body>
+      <!-- Navbar -->
+
+      <a href="#">
+         <h2>Page 2</h2>
+      </a>
+      
+   </body>
+
+</html>
